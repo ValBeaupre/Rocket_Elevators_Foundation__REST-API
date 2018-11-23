751fc69369f76dd3ebd08d8823e89aa63414ca0b
ValBeaupre <valbeaupre@hotmail.com>
2018-11-23 18:46:23 -0500

    Added usings in Batteries Controller

--- Controllers/BatteriesController.cs
+++ Controllers/BatteriesController.cs
@@ -1,4 +1,8 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json.Linq;
 using Rocket.Models;
 
